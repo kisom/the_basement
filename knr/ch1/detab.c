d3b68d104de0b36b5fe604afe658f2beb546b023
Kyle Isom <sadm@kyleisom.net>
2011-01-19 16:48:30 -0700

    updating detab

--- knr/ch1/detab.c
+++ knr/ch1/detab.c
@@ -9,4 +9,70 @@
  *                                                                        *
  **************************************************************************/
 
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "detab.h"
+
+int main(int argc, char **argv) {
+    string_t *buffer = calloc(1, sizeof *buffer);
+    string_init(buffer, INPUT_SIZE);
+    size_t cur = 0;
+    size_t tab = 0;
+
+    printf("input> ");
+    fgets(buffer->data, INPUT_SIZE - 1, stdin);
+
+    while ('\x00' != buffer->data[buffer->pos]) {
+        printf("pos: %u char: 0x%x\n", (unsigned int) buffer->pos,
+               (unsigned int) buffer->data[buffer->pos]);
+        if ('\t' == buffer->data[buffer->pos] ) { 
+            cur = buffer->pos;
+            tab++; 
+            while (EXIT_SUCCESS == lookahead(buffer, '\t', 1)) {
+                tab++;
+            }
+        }
+
+        if (tab) {
+            if (! sub_tabs(buffer, cur, tab)) { }
+        }
+        buffer->pos++;
+    }
+
+    printf("%u tabs detected...\n", (unsigned int) tab);
+
+    return EXIT_SUCCESS;
+}
+
+
+/* int lookahead() */
+int lookahead(string_t *buf, char target, int update) {
+    int result = EXIT_FAILURE;
+    if ( buf->pos < (buf->nm - 1) ) { 
+        if ( buf->data[buf->pos + 1] == target ) { 
+            printf("lookahead: success!\n");
+            result = EXIT_SUCCESS;
+        } else {
+            printf("lookahead: failed!\n");
+        }
+
+        if (update) { 
+            printf("update fired!\n");
+            buf->pos++; 
+        }
+    }
+
+    return result;
+}
+
+void string_init(string_t *buffer, size_t nm) {
+    buffer->data = calloc(nm, sizeof *buffer->data);
+    buffer->nm   = nm;
+    buffer->pos  = 0;
+}
+
+int sub_tabs(string_t *buf, size_t index, size_t tab_n) {
+    return 0;
+}
 
--- knr/ch1/detab.h
+++ knr/ch1/detab.h
@@ -16,15 +16,36 @@
 /***********/
 
 #define     TABSTOP         4
+#define     INPUT_SIZE      110     /* number of chars for input */
 
 
 /***********/
 /* structs */
 /***********/
 
-typedef struct string {
-    char *data;
-    size_t nm;
-} string_t, *pstring_t;
+struct string {
+    char *data;     /*  character buffer  */
+    size_t nm;      /* number of members  */
+    size_t pos;     /* position in buffer */
+};
 
+typedef struct string string_t; 
+
+
+/***********************/
+/* function prototypes */
+/***********************/
+
+/* lookahead: check the next character in the string to look for a specific
+ * character.
+ *      arguments:  string_t containing the string to search
+ *                  char containing the target character to find
+ *                  int set to nonzero if the position should be updated
+ *      returns: EXIT_SUCCESS if the character was found
+ *               EXIT_FAILURE if the character was not found
+ */
+int lookahead(string_t *buf, char target, int update);
+
+void string_init(string_t *buffer, size_t nm);
+int sub_tabs(string_t *buf, size_t index, size_t tab_n) ;
 
